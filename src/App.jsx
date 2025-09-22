18c18713de51f5d3d45f3377ebfdeabd7113c9d7
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 11:31:22 -0600

    import navbar and create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route></Route>
       </Routes>
+      
     </BrowserRouter>
   )
 }
