6ccc1d299c0ed6bd6efea3b3ac67db1672c9d09c
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 14:12:36 -0600

    add import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 function App() {
 
