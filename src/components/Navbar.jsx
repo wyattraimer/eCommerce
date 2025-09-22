c8c005badfb37c0892e76b036cfe8ef19335436d
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 14:40:54 -0600

    create different tabs section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,6 +27,20 @@ export const Navbar = (props) => {
                 </button>
             </div>
         </div>
+        <div className="flex items-center justify-center space-x-10 py-4 text-sm font-bold">
+            <Link to="/" className="hover:underline">
+                Home
+            </Link>
+            <Link to="/" className="hover:underline">
+                Shop
+            </Link>
+            <Link to="/" className="hover:underline">
+                Contact
+            </Link>
+            <Link to="/" className="hover:underline">
+                About
+            </Link>
+        </div>
     </nav>
     );
 }
