73e8709a1bf31682202bbf8908e2e4df7beb56ba
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 12:39:49 -0600

    import footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route></Route>
       </Routes>
-      
+      <Footer />
     </BrowserRouter>
   )
 }
