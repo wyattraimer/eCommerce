a153f8f10684abf79dfd9d20040c2d9caacece40
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 16:45:04 -0600

    created category comp. import images

---
+++ src/components/CategorySection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ManCategory from '../assets/Images/ManFashion.jpg'
+import WomanCategory from '../assets/Images/WomanFashion.jpg'
+import KidCategory from '../assets/Images/KidFashion.jpg'
+
+const CategorySection = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default CategorySection
