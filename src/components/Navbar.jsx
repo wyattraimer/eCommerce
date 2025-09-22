f9822f397861b3c5c723d6c3cd882320abff8f32
Wyatt Raimer <wyattraimer@gmail.com>
2025-09-22 14:33:30 -0600

    adding tailwind css to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaShoppingCart, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export const Navbar = (props) => {
     return (
-    <nav>
-        <div>
-            <div>
+    <nav className="bg=white shadow-md">
+        <div className="container mx-auto px-4 md:px-16 lg:px-24 py-4 flex justify-between items-center">
+            <div className="text-lg font-bold">
                 <Link to="/">eShop</Link>
             </div>
-            <div>
+            <div className="relative flex-1 mx-4">
                 <form>
-                    <input type="text" placeholder="Search Product"/>
-                    <FaSearch></FaSearch>
+                    <input type="text" placeholder="Search Product" className="w-full border py-2 px-4" />
+                    <FaSearch className="absolute top-3 right-3 text-red-500" />
                 </form>
+            </div>
+            <div className="flex items-center space-x-4">
+                <Link to="/cart">
+                    <FaShoppingCart />
+                </Link>
+                <button className="hidden md:block">
+                    Login | Register
+                </button>
+                <button className="block md:hidden">
+                    <FaUser />
+                </button>
             </div>
         </div>
     </nav>
