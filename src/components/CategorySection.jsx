30a1828a04e863543e5a67081fb5021302b1bd60
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-02 12:00:31 -0600

    add cards for category using map and add styling

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -3,10 +3,33 @@ import ManCategory from '../assets/Images/ManFashion.jpg'
 import WomanCategory from '../assets/Images/WomanFashion.jpg'
 import KidCategory from '../assets/Images/KidFashion.jpg'
 
+const categories = [
+  {
+    title: 'Men',
+    imageUrl: ManCategory,
+  },
+  {
+    title: 'Women',
+    imageUrl: WomanCategory,
+  },
+  {
+    title: 'Kids',
+    imageUrl: KidCategory,
+  },
+]
+
 const CategorySection = () => {
   return (
-    <div>
-
+    <div className='container mx-auto grid grid-cols-1 sm:grid-cols-3 gap-6'>
+      {categories.map((category, index) => (
+        <div key={index} className='relative h-64 transform transition-transform duration-300 hover:scale-105 cursor-pointer'>
+          <img src={category.imageUrl} alt='' className='w-full h-full object-cover rounded-lg shadow-md'/>
+          <div className='absolute top-20 left-12 '>
+            <p className='text-xl font-bold'>{category.title}</p>
+            <p className='text-gray-600'>View All</p>
+          </div>
+        </div>
+      ))}
     </div>
   )
 }
