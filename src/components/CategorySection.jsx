39b97cf1ce08db8a68f5bcca1914e67d451c6979
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-02 12:04:04 -0600

    style tweak

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -26,7 +26,7 @@ const CategorySection = () => {
           <img src={category.imageUrl} alt='' className='w-full h-full object-cover rounded-lg shadow-md'/>
           <div className='absolute top-20 left-12 '>
             <p className='text-xl font-bold'>{category.title}</p>
-            <p className='text-gray-600'>View All</p>
+            <p className='text-white-600'>View All</p>
           </div>
         </div>
       ))}
