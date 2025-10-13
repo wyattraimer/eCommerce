90376581a96b62a5cccf6bc2b1be48cb544f8aad
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-13 14:18:27 -0600

    implementing add to cart functionality

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { FaStar } from "react-icons/fa";
+import { addToCart } from '../redux/cartSlice';
+import { useDispatch } from 'react-redux';
 
 const ProductCard = ({product}) => {
+    const dispatch = useDispatch();
+    const handleAddToCart = (e, product) => {
+        e.stopPropagation()
+        e.preventDefault()
+        dispatch(addToCart(product))
+        alert("Product Added Successfully")
+    }
   return (
     <div className="bg-white p-4 shadow rounded relative border transform transition-transform duration-300 hover:scale-105">
         <img src={product.image} alt={product.name} className='w-full h-48 object-contain mb-4' />
@@ -13,7 +22,9 @@ const ProductCard = ({product}) => {
             <FaStar className='text-yellow-500'/>
             <FaStar className='text-yellow-500'/>
         </div>
-        <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transform transition-transform duration-300 hover:scale-105'>
+        <div className='absolute bottom-4 right-2 flex items-center justify-center w-8 h-8 bg-red-600 group text-white text-sm rounded-full hover:w-32 hover:bg-red-700 transition-all duration-100'
+             onClick={(e) => handleAddToCart(e, product)}
+        >
             <span className='group-hover:hidden'>+</span>
             <span className='hidden group-hover:block'>Add to cart</span>
         </div>
