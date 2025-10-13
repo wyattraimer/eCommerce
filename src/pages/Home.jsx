5cc20c5d0af2e15ab708e8d519f7d6caaedac3d2
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-13 12:03:32 -0600

    make shop in home fit with top products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,6 +16,7 @@ const Home = () => {
   }, [])
 
   return (
+    <div>
     <div className="bg-white mt-2 px-4 md:px-16 lg:px-24">
       <div className="container mx-auto py-4 flex flex-col md:flex-row space-x-2">
         <div className="w-full md:w-3/12 ">
@@ -49,7 +50,8 @@ const Home = () => {
           ))}
         </div>
       </div>
-      <Shop />
+    </div>
+    <Shop />
     </div>
   );
 };
