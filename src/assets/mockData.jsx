ebe463b2b51d8acc1fa7746814de4bd6cdbfc2bb
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-13 10:37:00 -0600

    importing product images and inserting into product array

--- src/assets/mockData.jsx
+++ src/assets/mockData.jsx
@@ -1,3 +1,14 @@
+import Image1 from "./Images/backpack.jpg";
+import Image2 from "./Images/bikini.jpg";
+import Image3 from "./Images/earbuds.jpg";
+import Image4 from "./Images/hat.jpg";
+import Image5 from "./Images/lure.jpg";
+import Image6 from "./Images/purse.jpg";
+import Image7 from "./Images/reel.jpg";
+import Image8 from "./Images/rings.jpg";
+import Image9 from "./Images/sneaker.jpg";
+import Image10 from "./Images/sunglasses.jpg";
+
 export const Categories = [
     "Electronics",
     "Fashion",
@@ -6,3 +17,66 @@ export const Categories = [
     "Sports",
     "Automotive",
 ];
+
+export const mockData = [
+    {
+        id: 1,
+        image: Image1,
+        name: "Product 1",
+        price: 29.99
+    },
+    {
+        id: 2,
+        image: Image2,
+        name: "Product 2",
+        price: 29.99
+    },
+    {
+        id: 3,
+        image: Image3,
+        name: "Product 3",
+        price: 29.99
+    },
+    {
+        id: 4,
+        image: Image4,
+        name: "Product 4",
+        price: 29.99
+    },
+    {
+        id: 5,
+        image: Image5,
+        name: "Product 5",
+        price: 29.99
+    },
+    {
+        id: 6,
+        image: Image6,
+        name: "Product 6",
+        price: 29.99
+    },
+    {
+        id: 7,
+        image: Image7,
+        name: "Product 7",
+        price: 29.99
+    },
+    {
+        id: 8,
+        image: Image8,
+        name: "Product 8",
+        price: 29.99
+    },
+    {
+        id: 9,
+        image: Image9,
+        name: "Product 9",
+        price: 29.99
+    },
+    {
+        id: 10,
+        image: Image10,
+        name: "Product 10",
+        price: 29.99
+    },
+]
