454ee7704b3d55f8f65d2a880d8034768178f939
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-13 11:46:42 -0600

    more css for productcards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,9 +40,9 @@ const Home = () => {
       <InfoSection />
       <CategorySection />
 
-      <div>
-        <h2>Top Products</h2>
-        <div>
+      <div className="container mx-auto py-12">
+        <h2 className="text-2xl font-bold mb-6 text-center">Top Products</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
           {products.products.slice(0,5).map((product) => (
             <ProductCard product={product} />
           ))}
