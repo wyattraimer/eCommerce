596c359fd1e41b198e5ff14ed86f2bc320bcc5f4
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-13 14:02:35 -0600

    tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,6 +43,15 @@ const Footer = () => {
           </form>
         </div>
       </div>
+      <div className='mt-8 border-t border-gray-700 pt-4'>
+        <div className='container mx-autp flex flex-col md:flex-row justify-between items-center'>
+          <p>&copy; 2024 eShop All rights reserved.</p>
+          <div className='flex space-x-6 mt-4 md:mt-0'>
+            <a href='' className='hover:underline'>Privacy Policy</a>
+            <a href='' className='hover:underline'>Terms & Conditions</a>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
