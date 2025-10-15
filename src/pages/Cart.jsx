b547182b6e3122b9f7ecd7bfbcf6de3b598ada2c
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-15 15:46:35 -0600

    making close button work on change address component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,12 @@ import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import EmptyCart from '../assets/Images/emptycart.png'
 import { FaTrashAlt } from 'react-icons/fa'
+import Modal from '../components/Modal';
 
 const Cart = () => {
     const cart = useSelector(state => state.cart);
     const [address, setAddress] = useState('1100 Main Street');
+    const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className='container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24'>
         {cart.products.length > 0 ?
@@ -65,7 +67,12 @@ const Cart = () => {
                                 Shipping to{" "}
                                 <span className='text-xs font-bold'>{address}</span>
                             </p>
-                            <button className='text-blue-500 hover:underline mt-1 ml-2'>Change Address</button>
+                            <button
+                                className='text-blue-500 hover:underline mt-1 ml-2'
+                                onClick={() => setIsModalOpen(true)}
+                            >
+                                Change Address
+                            </button>
                         </div>
                         <div className='flex justify-between mb-4'>
                             <span>Total Price:</span>
@@ -74,13 +81,19 @@ const Cart = () => {
                         <button className='w-full bg-red-600 text-white py-2 hover:bg-red-800'>Proceed to Checkout</button>
                     </div>
                 </div>
+                <Modal
+                    isModalOpen = {isModalOpen}
+                    setIsModalOpen = {setIsModalOpen}
+                >
+                </Modal>
             </div>
-            : <div className='flex justify-center'>
+            : (
+            <div className='flex justify-center'>
                 <img src={EmptyCart} className='h-96'/>
-              </div>
-        }
+            </div>
+        )}
     </div>
-  )
-}
+  );
+};
 
 export default Cart
