f7b5db66dc7ef49b63a50aebacbe99c55359a554
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-15 15:26:37 -0600

    cart total display and tailwind

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import EmptyCart from '../assets/Images/emptycart.png'
 import { FaTrashAlt } from 'react-icons/fa'
 
 const Cart = () => {
-    const cart = useSelector(state => state.cart)
+    const cart = useSelector(state => state.cart);
+    const [address, setAddress] = useState('1100 Main Street');
   return (
     <div className='container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24'>
         {cart.products.length > 0 ?
@@ -52,7 +53,26 @@ const Cart = () => {
                             ))}
                     </div>
                     
-                    <div></div>
+                    <div className='md:w-1/3 bg-white p-6 rounded-lg shadow-md border'>
+                        <h3 className='text-sm font-semibold mb-5'>CART TOTAL</h3>
+                        <div className='flex justify-between mb-5 border-b pb-1'>
+                            <span className='text-sm'>TOTAL ITEMS</span>
+                            <span>{cart.totalQuantity}</span>
+                        </div>
+                        <div className='mb-4 border-b pb-2'>
+                            <p>Shipping:</p>
+                            <p className='ml-2'>
+                                Shipping to{" "}
+                                <span className='text-xs font-bold'>{address}</span>
+                            </p>
+                            <button className='text-blue-500 hover:underline mt-1 ml-2'>Change Address</button>
+                        </div>
+                        <div className='flex justify-between mb-4'>
+                            <span>Total Price:</span>
+                            <span>${cart.totalPrice.toFixed(2)}</span>
+                        </div>
+                        <button className='w-full bg-red-600 text-white py-2 hover:bg-red-800'>Proceed to Checkout</button>
+                    </div>
                 </div>
             </div>
             : <div className='flex justify-center'>
