93488310d5f902b4f6bac0058d9e9d6b763c2ed6
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-15 15:33:02 -0600

    component setup and tailwind

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Modal = () => {
   return (
-    <div>Modal</div>
+    <div className='fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50'>
+        <div className='bg-white rounded-lg shadow-lg p-6 w-full max-w-md'>
+            <button
+                className='absolute top-4 right-4 text-gray-300 text-3xl'
+                onClick={onClose}
+            >
+            &times;
+            </button>
+            <div>Children</div>
+        </div>
+    </div>
   )
 }
 
