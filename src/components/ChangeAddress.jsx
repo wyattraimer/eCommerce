0417278a0e9a5876b23086f082f516a2f22cd8d3
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-15 15:36:12 -0600

    component setup and tailwind

--- src/components/ChangeAddress.jsx
+++ src/components/ChangeAddress.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const ChangeAddress = () => {
   return (
-    <div>ChangeAddress</div>
+    <div>
+        <input
+            type="text" 
+            placeholder='Enter a new address'
+            className='border p-2 w-full mb-4'
+        />
+        <div className='flex justify-end'>
+            <button className='bg-gray-500 text-white py-2 px-4 rounded mr-2'>
+                Cancel
+            </button>
+            <button className='bg-blue-500 text-white py-2 px-4 rounded'>
+                Save Address
+            </button>
+        </div>
+    </div>
   )
 }
 
