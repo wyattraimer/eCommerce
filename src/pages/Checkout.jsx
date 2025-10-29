7ff117b172802cabdbca6032365f9b8945fdd8c1
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-29 15:40:25 -0600

    add first drop down box for checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaAngleDown, FaAngleUp } from "react-icons/fa";
 
 const Checkout = () => {
+  const [billingToggle, setBillingToggle] = useState(true);
   return (
     <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
-      <h3 className="text-2xl font-semibold mb-4">Order Summary</h3>
+      <h3 className="text-2xl font-semibold mb-4">Checkout</h3>
       <div className="flex flex-col md:flex-row justify-between space-x-10 mt-8">
-        <div className="md:w-2/3"></div>
+        <div className="md:w-2/3">
+          <div className="border p-2 mb-6">
+            <div
+              className="flex items-center justify-between"
+              onClick={() => setBillingToggle(!billingToggle)}
+            >
+              <h3 className="text-lg font-semibold mb-2">
+                Billing Information
+              </h3>
+              {billingToggle ? <FaAngleDown /> : <FaAngleUp />}
+            </div>
+
+            <div className={`space-y-4 ${billingToggle ? "" : "hidden"}`}>
+              <div>
+                <label className="block text-gray-700">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Enter Name"
+                  className="w-full px-3 py-2 border"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <label className="block text-gray-700">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Email"
+                  className="w-full px-3 py-2 border"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <label className="block text-gray-700">Phone</label>
+                <input
+                  type="text"
+                  name="phone"
+                  placeholder="Enter Phone Number"
+                  className="w-full px-3 py-2 border"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border"></div>
       </div>
