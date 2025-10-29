2dadb08c579a670481d6c6219216cb180a9e4abd
Wyatt Raimer <wyattraimer@gmail.com>
2025-10-29 15:11:53 -0600

    Coppied code from Cart and removed not need code

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Checkout = () => {
   return (
-    <div>Checkout</div>
-  )
-}
+    <div className="container mx-auto py-8 min-h-96 px-4 md:px-16 lg:px-24">
+      <h3 className="text-2xl font-semibold mb-4">Order Summary</h3>
+      <div className="flex flex-col md:flex-row justify-between space-x-10 mt-8">
+        <div className="md:w-2/3"></div>
 
-export default Checkout
+        <div className="md:w-1/3 bg-white p-6 rounded-lg shadow-md border"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
