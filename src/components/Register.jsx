a05d112df7166ce6c199194226b831d540d03ff3
Wyatt Raimer <wyattraimer@gmail.com>
2025-11-03 15:51:31 -0700

    Register func and css

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Sign Up</h2>
+      <form>
+        <div className="mb-4">
+          <label className="block text-gray-700">Name</label>
+          <input
+            type="text"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter Name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Email</label>
+          <input
+            type="email"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter Email"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Password</label>
+          <input
+            type="password"
+            className="w-full px-3 py-2 border"
+            placeholder="Enter Password"
+          />
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="w-full bg-red-600 text-white py-2">
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <div className="text-center">
+        <span className="text-gray-700">Already Have an Account?</span>
+        <button className="text-red-800">Login</button>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
