6bb458ee6fa3c4e5a1c4c546adaa7f9cadaef6e9
Wyatt Raimer <wyattraimer@gmail.com>
2025-11-03 14:31:48 -0700

    Trying to troubleshoot Order page

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -2,38 +2,44 @@ import React from "react";
 
 const Order = ({ order }) => {
   return (
-    <div>
-      <h2>Thank you for your order.</h2>
+    <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
+      <h2 className="text-2x1 font-semibold mb-4">Thank you for your order.</h2>
       <p>
         Your order has been placed successfully. You will receive an email
         confirmation shortly.
       </p>
-      <div>
-        <h3>Order Summary</h3>
+      <div className="mt-6 p-4 border rounded-lg bg-gray-100">
+        <h3 className="text-lg font-semibold mb-2">Order Summary</h3>
         <p>Order Number: {order.orderNumber}</p>
-        <div>
-          <h2>Shipping Information</h2>
+        <div className="mt-4">
+          <h4 className="text-md font-semibold mb-2">Shipping Information</h4>
           <p>{order.shippingInformation.address}</p>
           <p>{order.shippingInformation.city}</p>
           <p>{order.shippingInformation.zip}</p>
         </div>
-        <div>
-          <h3>Products Ordered:</h3>
-          {order.products.map(product => (
-            <div>
-              <p>{product.name} x {product.quantity}</p>
-              <p>{product.price * product.quantity}</p>
+        <div className="mt-4">
+          <h4 className="text-md font-semibold mb-2">Items Ordered:</h4>
+          {order.products.map((product) => (
+            <div key={product.id} className="flex justify-between mt-2">
+              <p>
+                {product.name} (x{product.quantity})
+              </p>
+              <p>${product.price * product.quantity}</p>
             </div>
           ))}
         </div>
-        <div>
+        <div className="mt-4 flex justify-between">
           <span>Total Price:</span>
-          <span>{order.totalPrice}</span>
+          <span className="font-semibold">${order.totalPrice.toFixed(2)}</span>
         </div>
-        <div>
-          <button>Order Tracking</button>
-          <button>Continue Shopping</button>
-        </div>
+      </div>
+      <div className="mt-6">
+        <button className="bg-green-500 text-white py-2 px-4 hover:bg-green-600">
+          Track Order
+        </button>
+        <button className="ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800">
+          Continue Shopping
+        </button>
       </div>
     </div>
   );
