2af24e339092b5eca5bbc4b2e1de2e5065819718
Wyatt Raimer <wyattraimer@gmail.com>
2025-11-03 15:32:22 -0700

    navigate to home page with 'continue shopping' button

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Order = ({ order }) => {
+    const navigate = useNavigate()
   return (
     <div className="container mx-auto py-8 px-4 md:px-16 lg:px-24">
-      <h2 className="text-2x1 font-semibold mb-4">Thank you for your order.</h2>
+      <h2 className="text-2x1 font-semibold mb-4">Thank you for your order!</h2>
       <p>
-        Your order has been placed successfully. You will receive an email
-        confirmation shortly.
+        Your order has been placed successfully. You will receive a confirmation email shortly.
       </p>
       <div className="mt-6 p-4 border rounded-lg bg-gray-100">
-        <h3 className="text-lg font-semibold mb-2">Order Summary</h3>
+        <h3 className="text-lg font-semibold mb-2">Order Summary:</h3>
         <p>Order Number: {order.orderNumber}</p>
         <div className="mt-4">
-          <h4 className="text-md font-semibold mb-2">Shipping Information</h4>
+          <h4 className="text-md font-semibold mb-2">Shipping Information:</h4>
           <p>{order.shippingInformation.address}</p>
           <p>{order.shippingInformation.city}</p>
           <p>{order.shippingInformation.zip}</p>
@@ -37,7 +38,10 @@ const Order = ({ order }) => {
         <button className="bg-green-500 text-white py-2 px-4 hover:bg-green-600">
           Track Order
         </button>
-        <button className="ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800">
+        <button 
+            className="ml-4 bg-red-600 text-white py-2 px-4 hover:bg-red-800"
+            onClick={() => navigate('/')}
+        >
           Continue Shopping
         </button>
       </div>
